f9a81109f157f31d0d78b608dbfb0643d9d95635
Tuxdboy <86217113+Tuxdboy@users.noreply.github.com>
2022-10-21 16:57:59 -0500

    Update 1.lua

--- Blox Hunt/1.lua
+++ Blox Hunt/1.lua
@@ -14,25 +14,25 @@ function ESP()
         if v.Character:FindFirstChild("Object") then
             local Highlight = Instance.new("Highlight", v.Character.Object)
             Highlight.FillColor = Color3.fromRGB(255, 255, 0)
-        end
     end
     v.Character.ChildAdded:Connect(function(child)
         if child.Name == "Object" then
             local Highlight = Instance.new("Highlight", v.Character.Object)
             Highlight.FillColor = Color3.fromRGB(255, 255, 0)
-        end
-    end)
+            end
+        end)
+    end
     game:GetService("Players").PlayerAdded:Connect(function(player)
         if player.Character:FindFirstChild("Object") then
             local Highlight = Instance.new("Highlight", player.Character.Object)
             Highlight.FillColor = Color3.fromRGB(255, 255, 0)
-        end
-    end)
+    end
     player.Character.ChildAdded:Connect(function(child)
         if child.Name == "Object" then
             local Highlight = Instance.new("Highlight", player.Characterv.Object)
             Highlight.FillColor = Color3.fromRGB(255, 255, 0)
-        end
+            end
+        end)
     end)
 end
 
