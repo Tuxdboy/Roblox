c6e260a243bc0f8355dde9cd32aee20c1a2f6afc
Tuxdboy <86217113+Tuxdboy@users.noreply.github.com>
2022-10-30 16:38:53 -0500

    Update 2.lua

--- Lumber Tycoon 2/2.lua
+++ Lumber Tycoon 2/2.lua
@@ -67,6 +67,71 @@ function FindSpookWood()
     end
 end
 
+function FindSinisterWood()
+    repeat
+        wait(1)
+    until game:IsLoaded() --waits until game is loaded to execute script.
+    wait(1)
+    --clipboard function
+    function toClipboard(String)
+        local clipBoard = setclipboard or toclipboard or set_clipboard or (Clipboard and Clipboard.set)
+        if clipBoard then
+            clipBoard(String)
+            notify("Clipboard", "Copied to clipboard")
+        else
+            notify("Clipboard", "Your exploit doesn't have the ability to use the clipboard")
+        end
+    end
+    --spook tree finder
+    local ToFind = "Sinister"
+    local Tree
+    for i, v in pairs(workspace:GetChildren()) do
+        if v.Name == "TreeRegion" then
+            for l, c in pairs(v:GetChildren()) do
+                if c:FindFirstChild("TreeClass") then
+                    if c.TreeClass.Value == ToFind then
+                        Tree = c
+                        break
+                    end
+                end
+            end
+        end
+    end
+    --copies the jobid to clipboard, and notifies if there is a spooky tree
+    if Tree then
+        local r = game.Players.LocalPlayer.Character.HumanoidRootPart
+        r.CFrame = CFrame.new(Tree.WoodSection.Position)
+        game.StarterGui:SetCore(
+            "SendNotification",
+            {
+                Title = "Sinister Finder",
+                Text = "Found Sinister!\nJobID has been copied to clipboard.",
+                Duration = 15
+            }
+        )
+        local jobId = "Roblox.GameLauncher.joinGameInstance(" .. game.PlaceId .. ', "' .. game.JobId .. '")'
+        toClipboard(jobId)
+    else
+        if not tree then
+            --if there is no tree then it will notify that there isn't a tree, then it will serverhop.
+            wait(1.2)
+            game.StarterGui:SetCore(
+                "SendNotification",
+                {
+                    Title = "Sinister Finder",
+                    Text = "There is no Sinister here.",
+                    Duration = 10
+                }
+            )
+            print("There is no Sinister here.")
+            --serverhop script
+            local module =
+                loadstring(game:HttpGet "https://raw.githubusercontent.com/Tuxdboy/Roblox/main/Lumber%20Tycoon%202/1.lua")()
+            module:Teleport(game.PlaceId)
+        end
+    end
+end
+
 
 local Material = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/MaterialLua/master/Module.lua"))()
 
@@ -74,7 +139,7 @@ local NewUI = Material.Load({
 	Title = "Lumber Tycoon 2 — By Tuxdboy",
 	Style = 1,
 	SizeX = 300,
-	SizeY = 150,
+	SizeY = 185,
 	Theme = "Dark"
 })
 
@@ -93,5 +158,12 @@ local NewButton = NewPage.Button({
     Text = "Find Spook Wood",
     Callback = function()
         FindSpookWood()
+    end
+})
+
+local NewButton = NewPage.Button({
+    Text = "Find Sinister Wood",
+    Callback = function()
+        FindSinisterWood()
     end
 })
